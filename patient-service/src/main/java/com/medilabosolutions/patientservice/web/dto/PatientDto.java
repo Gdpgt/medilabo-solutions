5c1d9490917089b24fa08be5d8de6704664d4fe3
DE PUGET Guillaume <guillaume.de.puget@xpfibre.com>
2026-03-30 15:04:52 +0200

    feat: add PatientDto

---
+++ patient-service/src/main/java/com/medilabosolutions/patientservice/web/dto/PatientDto.java
@@ -0,0 +1,35 @@
+package com.medilabosolutions.patientservice.web.dto;
+
+import jakarta.validation.constraints.*;
+
+import com.medilabosolutions.patientservice.model.Genre;
+
+import java.time.LocalDate;
+
+
+public record PatientDto(
+
+        @NotBlank(message = "Le nom est obligatoire.")
+        @Size(max = 50, message = "Le nom ne doit pas dépasser 50 caractères.")
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\-' ]+$", message = "Le nom contient des caractères non autorisés.")
+        String nom,
+
+        @NotBlank(message = "Le prénom est obligatoire.")
+        @Size(max = 50, message = "Le prénom ne doit pas dépasser 50 caractères.")
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\-' ]+$", message = "Le prénom contient des caractères non autorisés.")
+        String prenom,
+
+        @Past(message = "La date de naissance ne peut pas être dans le futur.")
+        @NotNull(message = "La date de naissance est obligatoire.")
+        LocalDate dateNaissance,
+
+        @NotNull
+        Genre genre,
+
+        @Size(max = 100, message = "La taille de l'adresse ne doit pas dépasser 100 caractères.")
+        @Pattern(regexp = "^[a-zA-ZÀ-ÿŒœ0-9'’ ,./():+°-]*$", message = "L'adresse contient des caractères spéciaux non autorisés.")
+        String adresse,
+
+        @Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$", message = "Le téléphone doit être au format 000-000-0000.")
+        String telephone
+) {}
