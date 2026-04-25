0764175c86c3026b3e6e198a48b6edd0c627af55
Guillaume de Puget <guillaumedepuget@gmail.com>
2026-04-26 00:25:49 +0200

    feat: configure Spring Security

---
+++ patient-service/src/main/java/com/medilabosolutions/patientservice/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.medilabosolutions.patientservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .anyRequest()
+                                .authenticated()
+                )
+                .httpBasic(withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(AbstractHttpConfigurer::disable) // Disabled because Http Basic is stateless (no session cookie to exploit)
+                .build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(@Value("${medilabo.username}") String username,
+                                                 @Value("${medilabo.password}") String password,
+                                                 PasswordEncoder encoder) {
+        UserDetails user = User.builder()
+                .username(username)
+                .password(encoder.encode(password))
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
