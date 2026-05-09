c06da768d9ef950a9f4f789f5475f960698afe3e
Guillaume de Puget <guillaumedepuget@gmail.com>
2026-05-09 17:32:09 +0200

    chore: init evaluation-risque-service

---
+++ evaluation-risque-service/src/main/java/com/medilabosolutions/evaluationrisqueservice/EvaluationRisqueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.evaluationrisqueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationRisqueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationRisqueServiceApplication.class, args);
+    }
+
+}
---
+++ evaluation-risque-service/src/test/java/com/medilabosolutions/evaluationrisqueservice/EvaluationRisqueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.evaluationrisqueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationRisqueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
